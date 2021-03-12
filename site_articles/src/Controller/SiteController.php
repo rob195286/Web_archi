090d30af00b89eff55c175165265ffaaef64c05c
rob <195286@ecam.be>
2021-03-12 17:31:00 +0100

    ajout de la bd plus début page

--- site_articles/src/Controller/SiteController.php
+++ site_articles/src/Controller/SiteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,20 +11,49 @@ use Symfony\Component\Routing\Annotation\Route;
 class SiteController extends AbstractController
 {
     /**
-     * @Route("/site", name="site")
+     * @Route("/site", name="index")
      */
     public function index(): Response
     {
+        $repoB = $this-> getDoctrine()->getRepository(Book::class);
+        $repoF = $this-> getDoctrine()->getRepository(Film::class);
+        $books = $repoB-> findAll();
+        $i = 1;
+        $filmsP1 = array();
+        $filmsP2 = array();
+        foreach ($repoF-> findAll() as $value=>$film) {
+            if ($value%2 == 0) {
+            //if ($i%2 == 0) {
+                $filmsP1[$value] = $film;
+            }
+            elseif ($value%2 == 1) {
+            //elseif ($i%2 == 1) {
+                $filmsP2[$value] = $film;
+            }
+            $i++;
+        }
+
         return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+            'books' => $books,
+            'films' => $filmsP1
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('site/home.html.twig', [
             'controller_name' => 'SiteController',
         ]);
     }
     /**
-     * @Route("/site/k", name="sitek")
+     * @Route("/site/article", name="show_article")
      */
-    public function home(): Response
+    public function article(): Response
     {
-        return $this->render('site/index.html.twig', [
+        return $this->render('site/article.html.twig', [
             'controller_name' => 'SiteController',
         ]);
     }
